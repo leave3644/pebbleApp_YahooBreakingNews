db581f5dc882a3445f5a37082a6d01f955c3ff6d
grese <grese@yahoo-inc.com>
2014-07-24 13:56:47 -0700

    added scrolling view to show detailed text

--- yahoo_breaking_news/src/yahoo_breaking_news.c
+++ yahoo_breaking_news/src/yahoo_breaking_news.c
@@ -1,22 +1,49 @@
 #include "pebble.h"
 
 #define NUM_MENU_SECTIONS 1
-#define NUM_MENU_ITEMS 3
+#define NUM_MENU_ITEMS 10
 
 static Window *window;
 
-// This is a simple menu layer
+// Menu Layers: for list stories view
 static SimpleMenuLayer *simple_menu_layer;
-
-// A simple menu layer can have multiple sections
 static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
-
-// Each section is composed of a number of menu items
 static SimpleMenuItem menu_items[NUM_MENU_ITEMS];
+
+// Scrolling & Text Layers: for story view
+static ScrollLayer *scroll_layer;
+static TextLayer *text_layer;
+static InverterLayer *inverter_layer;
+static char scroll_text[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam tellus, fermentu  m quis vulputate quis, vestibulum interdum sapien. Vestibulum lobortis pellentesque pretium. Quisque ultricies purus e  u orci convallis lacinia. Cras a urna mi. Donec convallis ante id dui dapibus nec ullamcorper erat egestas. Aenean a m  auris a sapien commodo lacinia. Sed posuere mi vel risus congue ornare. Curabitur leo nisi, euismod ut pellentesque se  d, suscipit sit amet lorem. Aliquam eget sem vitae sem aliquam ornare. In sem sapien, imperdiet eget pharetra a, lacin  ia ac justo. Suspendisse at ante nec felis facilisis eleifend.";
+static const int vert_scroll_text_padding = 4;
+
 
 static bool special_flag = false;
 
 static int hit_count = 0;
+
+static void show_scrolling_detail_layer(){
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_frame(window_layer);
+    GRect max_text_bounds = GRect(0, 0, bounds.size.w, 2000);
+    // Initialize the simple menu layer
+    scroll_layer = scroll_layer_create(bounds);
+    scroll_layer_set_click_config_onto_window(scroll_layer, window);
+
+    text_layer = text_layer_create(max_text_bounds);
+    text_layer_set_text(text_layer, scroll_text);
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+
+    GSize max_size = text_layer_get_content_size(text_layer);
+    text_layer_set_size(text_layer, max_size);
+    scroll_layer_set_content_size(scroll_layer, GSize(bounds.size.w, max_size.h + vert_scroll_text_padding));
+
+    scroll_layer_add_child(scroll_layer, text_layer_get_layer(text_layer));
+    inverter_layer = inverter_layer_create(GRect(0, 28, bounds.size.w, 28));
+    scroll_layer_add_child(scroll_layer, inverter_layer_get_layer(inverter_layer));
+
+    layer_add_child(window_layer, scroll_layer_get_layer(scroll_layer));
+}
 
 // You can capture when the user selects a menu icon with a menu item select callback
 static void menu_select_callback(int index, void *ctx) {
@@ -25,7 +52,6 @@ static void menu_select_callback(int index, void *ctx) {
   // Mark the layer to be updated
   layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));
 }
-
 // You can specify special callbacks to differentiate functionality of a menu item
 static void special_select_callback(int index, void *ctx) {
   // Of course, you can do more complicated things in a menu item select callback
@@ -46,7 +72,10 @@ static void special_select_callback(int index, void *ctx) {
 
   // Mark the layer to be updated
   layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));
+  show_scrolling_detail_layer();
 }
+
+
 
 // This initializes the menu upon window load
 static void window_load(Window *window) {
